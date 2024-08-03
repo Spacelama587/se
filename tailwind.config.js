5594ff191434a4bde11d4aa7148d3659740ca19e
thevijeeth@gmail.com <thevijeeth@gmail.com>
2024-08-03 23:13:11 +0530

    m

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      animation: {
+        rotate: 'rotate 10s linear infinite',
+      },
+      keyframes: {
+        rotate: {
+          '0%': { transform: 'rotate(0deg) scale(10)' },
+          '100%': { transform: 'rotate(-360deg) scale(10)' },
+        },
+      },
+    },
+  },
+  plugins: [
+
+  ],
+}
